70f8aa5b4f68ae5624db22a8642bf306ab5e009b
Amon Ahmed <Amon.Shokhin.Ahmed@gmail.com>
2021-09-23 15:55:56 +0200

    Add barebones server node

---
+++ Server.gd
@@ -0,0 +1,26 @@
+extends Node
+
+var network = NetworkedMultiplayerENet.new()
+var port = 1909
+var max_players = 100
+
+func _ready():
+	start_server()
+
+
+func start_server():
+	network.create_server(port, max_players)
+	get_tree().set_network_peer(network)
+	print("Server started")
+	
+	network.connect("peer_connected", self, "_peer_connected")
+	network.connect("peer_disconnected", self, "_peer_disconnected")
+
+
+func _peer_connected(player_id):
+	print("Player with id: " + str(player_id)+ " connected.")
+	
+
+
+func _peer_disconnected(player_id):
+	print("Player with id: " + str(player_id)+ " disconnected.")
