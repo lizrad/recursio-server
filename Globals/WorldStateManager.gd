25f1a156b1122ed9f271d18371a3d64a52cfb743
Karl <karl.bittner@hexaquo.at>
2021-09-28 13:32:58 +0200

    Add more values to world state
    will be needed for interpolation/extrapolation

--- Globals/WorldStateManager.gd
+++ Globals/WorldStateManager.gd
@@ -1,18 +1,39 @@
 extends Node
 onready var Server = get_node("/root/Server")
 
+#
+# World States are structured like this:
+# {
+# 	"T": timestamp,
+# 	"S": {
+#		player_id: {
+# 			"P": position,
+# 			"V": velocity,
+# 			"A": acceleration,
+# 			"R": rotation,
+# 			"H": rotationvelocity
+# 		}
+# 	}
+# }
+#
+
 func _physics_process(delta):
-	if Server.player_amount>0:
+	if Server.player_amount > 0:
 		var world_state = define_world_state()
 		Server.send_world_state(world_state)
 
 func define_world_state():
 	var time = Server.get_server_time()
-	var player_states={}
+	var player_states = {}
+	
 	for player_id in PlayerManager.player_states:
-		player_states[player_id]=PlayerManager.player_states[player_id]["P"]
+		player_states[player_id] = PlayerManager.player_states[player_id]
+	
 	var world_state = {}
-	world_state["T"]=time
-	world_state["P"]=player_states
+	
+	world_state["T"] = time
+	world_state["S"] = player_states
+	
 	#TODO add other necessary information
+	
 	return world_state
