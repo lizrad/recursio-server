6e48dff2dfdf8903192c7ab12c5d2ace4ddd04bf
Amon Ahmed <Amon.Shokhin.Ahmed@gmail.com>
2021-10-03 19:56:07 +0200

    Apply autoformat

--- Players/CharacterBase.gd
+++ Players/CharacterBase.gd
@@ -1,8 +1,6 @@
 extends KinematicBody
 class_name CharacterBase
 
-
 onready var Server = get_node("/root/Server")
 var game_id := -1
 var player_id := -1
-
--- Room/PlayerManager.gd
+++ Room/PlayerManager.gd
@@ -15,7 +15,7 @@ var _current_spawn_point = 0
 
 func despawn_player(player_id):
 	player_states.erase(player_id)
-	for i in range (ghosts[player_id].size()):
+	for i in range(ghosts[player_id].size()):
 		ghosts[player_id][i].queue_free()
 	ghosts.erase(player_id)
 	players[player_id].queue_free()
@@ -23,13 +23,14 @@ func despawn_player(player_id):
 	for other_player_id in players:
 		Server.despawn_enemy_on_client(other_player_id, player_id)
 
+
 func spawn_player(player_id, game_id):
 	var spawn_point = _find_next_spawn_point()
 	var player = _player_scene.instance()
 	player.game_id = game_id
 	player.player_id = player_id
 	player.transform.origin = spawn_point
-	ghosts[player_id]=[]
+	ghosts[player_id] = []
 	add_child(player)
 
 	#triggering spawns of enemies on all clients
@@ -45,17 +46,21 @@ func spawn_player(player_id, game_id):
 	#TODO: move this to where it makes sense
 	start_recording()
 
+
 func start_recording():
 	for player_id in players:
 		players[player_id].start_recording()
+
 
 func stop_recording():
 	for player_id in players:
 		players[player_id].stop_recording()
 
+
 func create_ghosts():
 	for player_id in players:
 		create_ghost_from_player(players[player_id])
+
 
 func create_ghost_from_player(player):
 	var ghost = _ghost_scene.instance()
@@ -66,11 +71,13 @@ func create_ghost_from_player(player):
 	add_child(ghost)
 	Server.send_ghost_record(player.gameplay_record, player.player_id)
 
+
 func _find_next_spawn_point():
 	var spawn_point = _possible_spawn_points[_current_spawn_point]
 	#spawnpoints currently just switch between two positions
 	_current_spawn_point = 1 - _current_spawn_point
 	return spawn_point
+
 
 func update_player_state(player_id, player_state):
 	if player_states.has(player_id):
@@ -89,12 +96,14 @@ func update_dash_state(player_id, dash_state):
 
 var timer = 3
 var do_once = false
+
+
 func _physics_process(delta):
-	timer-=delta
+	timer -= delta
 	for player_id in player_states:
 		if players.has(player_id):
 			players[player_id].apply_player_state(player_states[player_id], delta)
-	if timer<=0 and not do_once:
+	if timer <= 0 and not do_once:
 		do_once = true
 		stop_recording()
 		create_ghosts()
